a67cc08d0e8c00c26da3caf9c7f0b969e10f96d5
Yegor <smegor55@gmail.com>
2023-06-19 15:03:02 +0300

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # filer
+This project is for file processing. It takes file and creates some 
+output file that will processed in specific way, depends on what plugin
+you have choosen.
---
+++ src/IStreamTransformer.h
@@ -0,0 +1,21 @@
+#ifndef __IStreamTransformer_h__
+#define __IStreamTransformer_h__
+
+#include <istream>
+#include <ostream>
+
+class IStreamTransformer{
+public:
+    virtual const char* type() = 0;
+    virtual void transform(std::istream& in, std::ostream& out) = 0;
+    virtual void retransform(std::istream& in, std::ostream& out) = 0;
+    virtual ~IStreamTransformer(){}
+    //can add possibility of configuration using vargs
+};
+
+typedef IStreamTransformer*(*loader)();
+
+#define DEFINE_PLUGIN(class_type, plugin_name) \
+    extern "C" {class_type* load##plugin_name();}
+
+#endif
